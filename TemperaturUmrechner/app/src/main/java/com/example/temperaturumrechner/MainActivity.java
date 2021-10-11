1d9bab8cbbdb767f99dbd05757a46076b8269eb4
BuildTools <unconfigured@null.spigotmc.org>
2021-10-11 11:25:47 +0200

    business logic

--- TemperaturUmrechner/app/src/main/java/com/example/temperaturumrechner/MainActivity.java
+++ TemperaturUmrechner/app/src/main/java/com/example/temperaturumrechner/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.temperaturumrechner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etCelsius;
     private EditText etFahrenheit;
     private EditText etKelvin;
     private Button btn;
+    private Float celsius;
+    private Float kelvin;
+    private Float fahrenheit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +28,44 @@ public class MainActivity extends AppCompatActivity {
         etKelvin = (EditText) findViewById(R.id.etKelvin);
 
 
-    }
-    private void getCelsius(){
 
+
+        etCelsius.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    getCelsius();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+    }
+
+
+    private void getCelsius(){
+        celsius = Float.parseFloat(String.valueOf(etCelsius.getText()));
     }
 
     private void celsiusTOkelvin(){
+        kelvin = celsius + 273.15f;
+        etKelvin.setText(String.valueOf(kelvin));
 
     }
     private void celsiusTOfahrenheit(){
-        // print fahrenheit (celsius * 1.8 + 32)
+        fahrenheit =  celsius * 1.8f + 32f;
+        etFahrenheit.setText(String.valueOf(fahrenheit));
+        // fahrenheit (celsius * 1.8 + 32)
     }
 }
